a19e12773a61ebf7428e44fbe3cf061c56e2686a
TutusBOT <barte77pl@gmail.com>
2022-06-24 21:54:31 +0200

    fix: hashrouter links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,22 +10,22 @@ function Navbar({ active }: Navbar) {
 			<nav className="fixed w-full flex justify-center gap-8 py-4 text-white text-2xl bg-black z-20">
 				<AnchorUnderlined
 					body="HOME"
-					link="/"
+					link="/dbd-roulette/"
 					active={active === "home" ? true : false}
 				/>
 				<AnchorUnderlined
 					body="PERKS"
-					link="/#/perks"
+					link="/dbd-roulette/#/perks"
 					active={active === "perks" ? true : false}
 				/>
 				<AnchorUnderlined
 					body="KILLERS"
-					link="/#/killers"
+					link="/dbd-roulette/#/killers"
 					active={active === "killers" ? true : false}
 				/>
 				<AnchorUnderlined
 					body="REALMS"
-					link="/#/realms"
+					link="/dbd-roulette/#/realms"
 					active={active === "realms" ? true : false}
 				/>
 			</nav>
