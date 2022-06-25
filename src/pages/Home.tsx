ddb2245ad6c1c9240235843944715b01eb0fc1f6
TutusBOT <barte77pl@gmail.com>
2022-06-25 23:52:02 +0200

    feat: added basic description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,13 @@ function Home() {
 	return (
 		<>
 			<Navbar active="home" />
+			<main>
+				<h2>What is this all about?</h2>
+				<p>
+					This website is a randomizer in a shape of slot machine for Dead by
+					Daylight game.
+				</p>
+			</main>
 		</>
 	);
 }
